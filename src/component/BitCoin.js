649663fa2cf5894679e7224a63599fe8d9e74a0e
linhdoan9513 <doanlinh9513@gmail.com>
2021-08-09 19:25:05 -0400

    color-change-update

--- src/component/BitCoin.js
+++ src/component/BitCoin.js
@@ -1,38 +1,46 @@
-import React from 'react';
-
+import React from "react";
 
 class BitCoin extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            prevPrice: this.props.coin,
-            color: false
-        }
-    }
-   
-    componentDidUpdate(prevProps) {
-        if (prevProps.coin !== this.props.coin) {
-            this.setState({ 
-                prevPrice: prevProps.coin,
-                color: true
-            });
-            setTimeout(() => {
-                this.setState({color: false})
-            }, 1000)
-        } 
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      prevPrice: this.props.coin,
+      color: false,
+    };
+  }
 
-    componentWillUnmount() {
-        clearTimeout(this.changeColor)
+  componentDidUpdate(prevProps) {
+    if (prevProps.coin !== this.props.coin) {
+      this.setState({
+        prevPrice: prevProps.coin,
+        color: true,
+      });
+      setTimeout(() => {
+        this.setState({ color: false });
+      }, 1000);
     }
+  }
 
-    render() {
-        const { prevPrice } = this.state
-        if (this.props.coin > prevPrice) { 
-            return <h3 className={`${this.state.color ? "green" : ""}`}>{this.props.coin.toLocaleString()}</h3>
-        } 
-        return <h3 className={`${this.state.color ? "green" : ""}`}>{this.props.coin.toLocaleString()}</h3>
+  componentWillUnmount() {
+    clearTimeout(this.changeColor);
+  }
+
+  render() {
+    const { prevPrice } = this.state;
+    if (this.props.coin > prevPrice) {
+      return (
+        <h3 className={`${this.state.color ? "green" : ""}`}>
+          {this.props.coin.toLocaleString()}
+        </h3>
+      );
+    } else {
+      return (
+        <h3 className={`${this.state.color ? "red" : ""}`}>
+          {this.props.coin.toLocaleString()}
+        </h3>
+      );
     }
+  }
 }
 
 export default BitCoin;
